57703eb490362d3812e8a6012deacd98836610de
changkwon <ckcho231@gmail.com>
2022-08-21 21:23:27 +0900

    Changes

--- test3.py
+++ test3.py
@@ -7,8 +7,9 @@ def fib(n):  # write Fibonacci series up to n
         # a, b = b, a + b
         a = b
         b = a + b
-    # print()
+    print()
 
+# 안녕하세요 만나서 반가워요 이런일을 좋아하는것 아니였어요?
 
 # Now call the function we just defined:
 fib(100)
